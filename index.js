ef4f4f9ffd756de49414c42fc10a57add0d620d5
William Milanez <william_milanez@hotmail.com>
2024-09-15 13:49:27 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -53,13 +53,30 @@ const metasRealizadas = async () => {
     })
 
     if(realizadas.length == 0) {
-        console.log('Não existem metas realizdas! :/')
+        console.log('Não existem metas realizadas! :/')
         return
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+        // outra opção para ter o mesmo resultado é: return !meta.checked
+    })
+
+    if(abertas.length == 0) {
+        console.log("Não existem metas abertas! :D")
+        return
+    }
+
+    await select({
+        message: "Metas abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -83,6 +100,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -99,6 +120,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
